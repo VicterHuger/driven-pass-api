3759227f37fc6913f271018a43cd43d44e3f7674
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-09-06 10:52:43 -0300

    chore: add a provisional database to development tests

---
+++ src/config/databse.ts
@@ -0,0 +1,8 @@
+import {ISignUpBody} from '../types/userTypes';
+const users: ISignUpBody[] = [ 
+    {
+        email:"teste@gmail.com",
+        password: "1234567890"
+    }
+]
+export default users;
